fbd217c57d1bf991bc92f41c9ed6e2d567e75241
Huiba Li <lihuiba@gmail.com>
2014-04-11 05:18:13 -0700

    insmod argument

--- test-setup.sh
+++ test-setup.sh
@@ -6,6 +6,6 @@ dd if=/dev/zero of=slow bs=1M count=0 seek=1k
 dd if=/dev/zero of=fast bs=1M count=0 seek=1k
 losetup /dev/loop0 slow
 losetup /dev/loop1 fast
-insmod ./dm-foolcache.ko
+insmod ./dm-foolcache.ko async=1
 echo "0 `blockdev --getsize /dev/loop0` foolcache /dev/loop0 /dev/loop1 1024 create" | dmsetup create fcdev
 
